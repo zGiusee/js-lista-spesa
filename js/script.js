943daced1c9461b04892436db85cfb14a4841cb1
zGiusee <giusebelforte@gmail.com>
2023-11-27 13:37:02 +0100

    [UPDATE] exercise done

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+// CREO L'ARRAY CHE CONTENGA GLI INGREDIENTI
+const list = ["Uova", "Precorino", "Guanciale", "Pepe", "Pasta"];
+
+// INDICO LA LISTA NEL FILE HTML
+const list_container = document.getElementById("lista_della_spesa");
+
+let i = 0;
+
+// ESEGUO IL CICLO CHE MOSTRI NEL DOCUMENTO HTML I COMPONENTI DELL'ARRAY
+while(i < list.length){
+
+    let li = `<li>${list[i]}</li>`;
+
+    list_container.innerHTML += li;
+
+    i++;
+
+}
+
