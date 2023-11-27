1804524b1c1b0812e7328568f1bb9f360688e358
zGiusee <giusebelforte@gmail.com>
2023-11-27 16:55:19 +0100

    [UPDATE] added appenChild version to exercise

--- js/script.js
+++ js/script.js
@@ -8,13 +8,25 @@ const list_container = document.getElementById("lista_della_spesa");
 let i = 0;
 
 // ESEGUO IL CICLO CHE MOSTRI NEL DOCUMENTO HTML I COMPONENTI DELL'ARRAY
+// while(i < list.length){
+
+//     let li = `<li>${list[i]}</li>`;
+
+//     list_container.innerHTML += li;
+
+//     i++;
+
+// }
+
+// VERSIONE CON APPENCHILD
 while(i < list.length){
 
-    let li = `<li>${list[i]}</li>`;
+    let li = document.createElement("li");
+    li.innerText = list[i];
 
-    list_container.innerHTML += li;
+    list_container.appendChild(li);
 
     i++;
-
 }
+
 
